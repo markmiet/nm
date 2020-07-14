ea528d1971dd74b7cbc5281b9b01276eb8872d3a
markmiet <minamake@hotmail.com>
2020-07-14 16:20:12 +0300

    dd

--- Assets/Scripts/AlusController.cs
+++ Assets/Scripts/AlusController.cs
@@ -2,161 +2,147 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AlusController : MonoBehaviour
-{
+public class AlusController : MonoBehaviour {
 
-    private Rigidbody2D m_Rigidbody2D;
+	private Rigidbody2D m_Rigidbody2D;
 
-    //vauhti
+	//vauhti
+	private Animator m_Animator;
 
-    private Animator m_Animator;
+	private float oikeaNappiPainettu = 0.0f;
 
-    private float oikeaNappiPainettu = 0.0f;
+	private float vauhtiOikea = 0.0f;
+	private float vauhtiOikeaMax = 4.0f;
 
-    private float vauhtiOikea = 0.0f;
-    private float vauhtiOikeaMax = 4.0f;
+	private float hidastuvuusKunMitaanEiPainettu = 0.3f;
+	private float nopeudenMuutosKunPainettu = 1f;
+	//ylos/alla
 
+	private float ylosNappiPainettu = 0.0f;
 
-    private float hidastuvuusKunMitaanEiPainettu = 0.3f;
-    private float nopeudenMuutosKunPainettu = 1f;
-    //ylos/alla
+	private float vauhtiYlos = 0.0f;
+	private float vauhtiYlosMax = 4.0f;
 
-
-    private float ylosNappiPainettu = 0.0f;
-
-    private float vauhtiYlos = 0.0f;
-    private float vauhtiYlosMax = 4.0f;
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        m_Rigidbody2D= GetComponent<Rigidbody2D>();
-        m_Animator = GetComponent<Animator>();
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        oikeaNappiPainettu = Input.GetAxisRaw("Horizontal"); // > 0 for right, < 0 for left
-        ylosNappiPainettu = Input.GetAxisRaw("Vertical"); // > 0 for right, < 0 for left
+	private bool spaceNappiaPainettu = false;
+	private bool spaceNappiAlhaalla = false;
+	private bool spaceNappiYlhaalla = false;
 
 
 
-    }
+	private bool ammusInstantioitiinviimekerralla = false;
 
-    void FixedUpdate()
-    {
+	public GameObject ammusPrefab;
 
-        if (oikeaNappiPainettu == 0.0f)
-        {
-            //kumpaakaan ei painettu joten vaakasuunta nopeutta pitää vaan vähentä
-            if (vauhtiOikea > 0)
-            {
-                vauhtiOikea = vauhtiOikea - hidastuvuusKunMitaanEiPainettu;
-                if (vauhtiOikea < 0)
-                {
-                    vauhtiOikea = 0.0f;
-                }
-            }
-            else if (vauhtiOikea < 0)
-            {
-                vauhtiOikea = vauhtiOikea + hidastuvuusKunMitaanEiPainettu;
-                if (vauhtiOikea > 0.0f)
-                {
-                    vauhtiOikea = 0.0f;
-                }
-            }
-        }
-        else if (oikeaNappiPainettu > 0)
-        {
-            vauhtiOikea = vauhtiOikea + nopeudenMuutosKunPainettu;
-        }
-        else
-        {
-            //vasen painettu
-            vauhtiOikea = vauhtiOikea - nopeudenMuutosKunPainettu;
-        }
+	private SpriteRenderer m_SpriteRenderer;
 
 
-        if (vauhtiOikea > 0 && vauhtiOikea > vauhtiOikeaMax)
-        {
-            vauhtiOikea = vauhtiOikeaMax;
-        }
-        else if (vauhtiOikea < 0 && vauhtiOikea <= -(vauhtiOikeaMax))
-        {
-            vauhtiOikea = -(vauhtiOikeaMax);
-        }
+	// Start is called before the first frame update
+	void Start ()
+	{
+		m_Rigidbody2D = GetComponent<Rigidbody2D> ();
+		m_Animator = GetComponent<Animator> ();
 
-        //alas/ylös
-        if (ylosNappiPainettu == 0.0f)
-        {
-            //kumpaakaan ei painettu joten vaakasuunta nopeutta pitää vaan vähentä
-            if (vauhtiYlos > 0)
-            {
-                vauhtiYlos = vauhtiYlos - hidastuvuusKunMitaanEiPainettu;
-                if (vauhtiYlos < 0)
-                {
-                    vauhtiYlos = 0.0f;
-                }
-            }
-            else if (vauhtiYlos < 0)
-            {
-                vauhtiYlos = vauhtiYlos + hidastuvuusKunMitaanEiPainettu;
-                if (vauhtiYlos > 0.0f)
-                {
-                    vauhtiYlos = 0.0f;
-                }
-            }
-        }
-        else if (ylosNappiPainettu > 0)
-        {
-            vauhtiYlos = vauhtiYlos + nopeudenMuutosKunPainettu;
-        }
-        else
-        {
-            //vasen painettu
-            vauhtiYlos = vauhtiYlos - nopeudenMuutosKunPainettu;
-        }
+		m_SpriteRenderer = GetComponent<SpriteRenderer> ();
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		oikeaNappiPainettu = Input.GetAxisRaw ("Horizontal"); // > 0 for right, < 0 for left
+		ylosNappiPainettu = Input.GetAxisRaw ("Vertical"); // > 0 for right, < 0 for left
+		spaceNappiaPainettu = Input.GetButton ("Jump");
+		spaceNappiAlhaalla = Input.GetButtonDown ("Jump");
+		spaceNappiYlhaalla = Input.GetButtonUp ("Jump");
 
 
-        if (vauhtiYlos > 0 && vauhtiYlos > vauhtiYlosMax)
-        {
-            vauhtiYlos = vauhtiYlosMax;
-        }
-        else if (vauhtiYlos < 0 && vauhtiYlos <= -(vauhtiYlosMax))
-        {
-            vauhtiYlos = -(vauhtiYlosMax);
-        }
+	}
 
-         m_Rigidbody2D.velocity = new Vector2(vauhtiOikea, vauhtiYlos);
+	void FixedUpdate ()
+	{
 
-        //        m_Rigidbody2D
+		if (oikeaNappiPainettu == 0.0f) {
+			//kumpaakaan ei painettu joten vaakasuunta nopeutta pitää vaan vähentä
+			if (vauhtiOikea > 0) {
+				vauhtiOikea = vauhtiOikea - hidastuvuusKunMitaanEiPainettu;
+				if (vauhtiOikea < 0) {
+					vauhtiOikea = 0.0f;
+				}
+			} else if (vauhtiOikea < 0) {
+				vauhtiOikea = vauhtiOikea + hidastuvuusKunMitaanEiPainettu;
+				if (vauhtiOikea > 0.0f) {
+					vauhtiOikea = 0.0f;
+				}
+			}
+		} else if (oikeaNappiPainettu > 0) {
+			vauhtiOikea = vauhtiOikea + nopeudenMuutosKunPainettu;
+		} else {
+			//vasen painettu
+			vauhtiOikea = vauhtiOikea - nopeudenMuutosKunPainettu;
+		}
 
-        if (vauhtiYlos>0.0f)
-        {
-            m_Animator.SetBool("up", true);
+		if (vauhtiOikea > 0 && vauhtiOikea > vauhtiOikeaMax) {
+			vauhtiOikea = vauhtiOikeaMax;
+		} else if (vauhtiOikea < 0 && vauhtiOikea <= -(vauhtiOikeaMax)) {
+			vauhtiOikea = -(vauhtiOikeaMax);
+		}
 
-        }
-        else
-        {
-            m_Animator.SetBool("up", false);
-        }
-       
+		//alas/ylös
+		if (ylosNappiPainettu == 0.0f) {
+			//kumpaakaan ei painettu joten vaakasuunta nopeutta pitää vaan vähentä
+			if (vauhtiYlos > 0) {
+				vauhtiYlos = vauhtiYlos - hidastuvuusKunMitaanEiPainettu;
+				if (vauhtiYlos < 0) {
+					vauhtiYlos = 0.0f;
+				}
+			} else if (vauhtiYlos < 0) {
+				vauhtiYlos = vauhtiYlos + hidastuvuusKunMitaanEiPainettu;
+				if (vauhtiYlos > 0.0f) {
+					vauhtiYlos = 0.0f;
+				}
+			}
+		} else if (ylosNappiPainettu > 0) {
+			vauhtiYlos = vauhtiYlos + nopeudenMuutosKunPainettu;
+		} else {
+			//vasen painettu
+			vauhtiYlos = vauhtiYlos - nopeudenMuutosKunPainettu;
+		}
 
-        //Debug.Log("vauhtiOikea=" + vauhtiOikea);
-        //Debug.Log("vauhtiYlos=" + vauhtiYlos);
-      
+		if (vauhtiYlos > 0 && vauhtiYlos > vauhtiYlosMax) {
+			vauhtiYlos = vauhtiYlosMax;
+		} else if (vauhtiYlos < 0 && vauhtiYlos <= -(vauhtiYlosMax)) {
+			vauhtiYlos = -(vauhtiYlosMax);
+		}
 
-        AnimatorClipInfo[] m_CurrentClipInfo = this.m_Animator.GetCurrentAnimatorClipInfo(0);
+		m_Rigidbody2D.velocity = new Vector2 (vauhtiOikea, vauhtiYlos);
 
-        //Access the current length of the clip
-        //m_CurrentClipLength = m_CurrentClipInfo[0].clip.length;
-        //Access the Animation clip name
-        string ff  = m_CurrentClipInfo[0].clip.name;
-        Debug.Log("ff=" + ff);
+		//        m_Rigidbody2D
+		if (vauhtiYlos > 0.0f) {
+			m_Animator.SetBool ("up", true);
 
-    }
+		} else {
+			m_Animator.SetBool ("up", false);
+		}
+
+		//Debug.Log("vauhtiOikea=" + vauhtiOikea);
+		//Debug.Log("vauhtiYlos=" + vauhtiYlos);
+		Debug.Log ("spaceNappiYlhaalla=" + spaceNappiYlhaalla);
+
+		//m_Rigidbody2D.position.
+		bool ammusinstantioitiin = false;
+		//tee vaan rämpytyksestä parempi...
+		//tai sitten rajoitettu määrä ammuksia...
+
+		if (spaceNappiaPainettu) {
+			if (!ammusInstantioitiinviimekerralla) {
+				GameObject instanssi = Instantiate (ammusPrefab, new Vector3 (
+				m_Rigidbody2D.position.x + (m_SpriteRenderer.bounds.size.x / 2), m_Rigidbody2D.position.y, 0), Quaternion.identity);
+				instanssi.GetComponent<Rigidbody2D> ().velocity = new Vector2 (20, 0);
+				ammusinstantioitiin = true;
+			}
+		}
+		ammusInstantioitiinviimekerralla = ammusinstantioitiin;
+
+	}
+
 }
